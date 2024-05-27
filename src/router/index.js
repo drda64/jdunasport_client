1521171d8992013a51f60840a1893d46bad2ceec
Šimon Drda <drdasimo@fel.cvut.cz>
2024-05-27 21:44:40 +0200

    adding before each to check auth store token

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import EventView from '../views/EventView.vue'
+import {useTokenStore} from "@/stores/token.js";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,17 +9,46 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: '/events',
       name: 'events',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: EventView
+      component: EventView,
+      meta: {
+          requiresAuth: true
+      }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import(/* webpackChunkName: "login" */ '../views/LoginView.vue')
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: '404',
+      component: () => import(/* webpackChunkName: "404" */ '../views/NotFoundView.vue'),
+      meta: {
+        requiresAuth: true
+      }
     }
   ]
 })
+
+// Navigation guard to check authentication status
+router.beforeEach((to, from, next) => {
+  const authStore = useTokenStore();
+  if (to.matched.some(record => record.meta.requiresAuth) && !authStore.isAuthenticated) {
+    // Redirect to login page if authentication is required but user is not authenticated
+    next('/login');
+  } else {
+    // Continue with navigation
+    console.log('User is authenticated');
+    next();
+  }
+});
 
 export default router
