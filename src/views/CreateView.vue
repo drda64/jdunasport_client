ec56b80e79bc850d922abde32ba399c2b19a12cf
Šimon Drda <drdasimo@fel.cvut.cz>
2024-05-28 17:02:34 +0200

    refs for form

--- src/views/CreateView.vue
+++ src/views/CreateView.vue
@@ -1,6 +1,23 @@
 <script setup>
 import { ref } from 'vue';
 import { useTokenStore } from '../stores/token.js';
+
+// categories array ref
+const categories = ref([]);
+const categoryName = ref('');
+const capacity = ref(0);
+
+// form inputs for event
+const name = ref('');
+const description = ref('');
+const date = ref('');
+const time = ref('');
+const location = ref('');
+
+function addCategory() {
+  categories.value.push({ name: categoryName.value, capacity: capacity.value });
+}
+
 </script>
 
 <template>
