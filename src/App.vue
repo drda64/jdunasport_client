0a68e460217d431398d34745324c8993eb6a97d0
Šimon Drda <drdasimo@fel.cvut.cz>
2024-05-28 16:58:29 +0200

    adding styles

--- src/App.vue
+++ src/App.vue
@@ -16,4 +16,28 @@ import { RouterLink, RouterView } from 'vue-router'
 </template>
 
 <style scoped>
+  #app-container {
+    max-width: 50rem;
+    width: 100%;
+
+    padding-left: 2rem;
+    padding-right: 2rem;
+  }
+
+  #view {
+    margin-top: 3rem;
+  }
+
+  nav {
+    margin-top: 5rem;
+    display: flex;
+    gap: 2.5rem;
+
+    font-size: 0.9rem;
+  }
+
+  nav a {
+    text-decoration: none;
+    color: #000;
+  }
 </style>
