68b353d2a77ae60c9b55340a70cbd01090ed87ae
Šimon Drda <drdasimo@fel.cvut.cz>
2024-05-29 21:05:35 +0200

    init select component

---
+++ src/components/base/SelectComponent.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+import {ref} from "vue";
+
+const props = defineProps({
+  modelValue : [String, Number],
+  label : String,
+  options : Array,
+})
+
+const emit = defineEmits(['update:modelValue']);
+
+const inputValue = ref(props.modelValue);
+
+emit('update:modelValue', 1);
+
+const updateValue = (event) => {
+  emit('update:modelValue', event.target.value);
+  inputValue.value = event.target.value;
+  console.log(inputValue.value);
+};
+</script>
+
+<template>
+  <div>
+    <label>{{ label }}</label>
+    <select @change="updateValue">
+      <option v-for="option in options" :key="option.value" :value="option.value">{{ option.text }}</option>
+    </select>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
