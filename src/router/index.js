d66d7657caa03f06ad398df22b7218ccb698e701
Šimon Drda <drdasimo@fel.cvut.cz>
2024-05-29 21:05:11 +0200

    changing home to dashboard

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import EventView from '../views/EventView.vue'
 import {useTokenStore} from "@/stores/token.js";
+import DashboardView from "@/views/DashboardView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'dashboard',
+      component: DashboardView,
       meta: {
         requiresAuth: true
       }
