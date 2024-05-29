8a3154bd3bbbfcf93e6e126b07912f2a05603150
Šimon Drda <drdasimo@fel.cvut.cz>
2024-05-29 21:04:43 +0200

    init dashboard view

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,25 @@
+<script setup>
+import EventItem from "@/models/EventItem.js";
+import { ref, onMounted } from "vue";
+
+const events = ref([]);
+
+onMounted(async () => {
+  events.value = await EventItem.getAllEvents();
+});
+</script>
+
+<template>
+  <div>
+    <h1>Dashboard</h1>
+    <div v-for="event in events" :key="event.id">
+      <p>{{ event.name }}</p>
+      <p>{{ event.location }}</p>
+      <p>{{ event.description }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+/* Add your styles here */
+</style>
