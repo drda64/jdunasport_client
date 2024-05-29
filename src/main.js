81f937f0bc7688f304a8c380b7a94d84c68076e4
Šimon Drda <drdasimo@fel.cvut.cz>
2024-05-29 09:13:42 +0200

    importing notifications

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@ import App from './App.vue'
 import { createPinia } from 'pinia'
 import router from './router'
 import ('@/assets/main.css')
+import Notifications from '@kyvg/vue3-notification'
+import { setLocale } from 'yup';
 
 const app = createApp(App)
 const pinia = createPinia()
 
 app.use(pinia)
 app.use(router)
+app.use(Notifications)
 
 app.mount('#app')
