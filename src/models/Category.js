2ebc168050a1922b33a0023191f4e7472fd96d03
Šimon Drda <drdasimo@fel.cvut.cz>
2024-05-30 22:18:18 +0200

    importing axios and adding id

--- src/models/Category.js
+++ src/models/Category.js
@@ -1,5 +1,9 @@
-class Category {
-    constructor(name, capacity) {
+import axios from "axios";
+import {Participant} from "@/models/Participant.js";
+
+export default class Category {
+    constructor(id, name, capacity, participants = []) {
+        this.id = id;
         this.name = name;
         this.capacity = capacity;
     }
