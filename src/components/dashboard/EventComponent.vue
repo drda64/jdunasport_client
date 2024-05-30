10bd66bdb64b6fdf8a91206c97c2e1b259a426b9
Šimon Drda <drdasimo@fel.cvut.cz>
2024-05-30 22:18:50 +0200

    new designed event component

--- src/components/dashboard/EventComponent.vue
+++ src/components/dashboard/EventComponent.vue
@@ -1,11 +1,83 @@
 <script setup>
 
+defineProps({
+  event: Object
+})
+
 </script>
 
 <template>
-
+  <div class="container">
+    <div class="left-group">
+      <div class="top-group">
+        <h3>{{ event.name }}</h3>
+        <p>{{ event.date }} {{ event.time }}</p>
+      </div>
+      <div class="bottom-group">
+        <p><mark>{{ event.sport }}</mark></p>
+      </div>
+    </div>
+    <div class="right-group">
+      <div class="green dot"></div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
+.container {
+  flex: 1 1 calc(33.333% - 20px);
 
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  height: 8rem;
+  padding: 1.5rem;
+
+  background-color: #FAFAFA;
+}
+
+.left-group {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.dot {
+  width: 0.7rem;
+  height: 0.7rem;
+  border-radius: 50%;
+  box-sizing: border-box;
+}
+
+.green {
+  background-color: #1FC974;
+}
+
+.container:hover {
+  background-color: #b2e7da;
+  cursor: pointer;
+}
+
+.container:hover mark {
+  background-color: #000000;
+  color: white
+
+}
+
+p {
+  font-size: 0.9rem;
+}
+
+/* Responsive adjustments for smaller screens */
+@media (max-width: 1480px) {
+  .container {
+    flex: 1 1 calc(50% - 20px);
+  }
+}
+
+@media (max-width: 680px) {
+  .container {
+    flex: 1 1 calc(100% - 20px);
+  }
+}
 </style>
