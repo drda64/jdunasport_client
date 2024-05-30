cfde34963e192ee22742b549d9206aaa54e5eb01
Šimon Drda <drdasimo@fel.cvut.cz>
2024-05-30 22:21:23 +0200

    logging

--- src/stores/token.js
+++ src/stores/token.js
@@ -18,39 +18,14 @@ export const useTokenStore = defineStore('auth', {
             try {
                 const response = await axios.post('http://127.0.0.1:8009/login', user);
                 const token = response.data.access_token;
-                const refreshToken = response.data.refresh_token;
                 localStorage.setItem('token', token);
-                localStorage.setItem('refresh_token', refreshToken);
                 this.token = token;
-                this.refreshToken = refreshToken;
 
                 console.log('Login successful');
-                console.log(token)
-                console.log(refreshToken)
+                console.log(token);
             } catch (error) {
                 localStorage.removeItem('token');
-                localStorage.removeItem('refresh_token');
                 throw new Error('Invalid username or password');
-            }
-        },
-        async refreshToken() {
-            try {
-                const refreshToken = localStorage.getItem('refresh_token');
-                if (!refreshToken) {
-                    throw new Error('No refresh token available');
-                }
-                axios.defaults.headers.common['Authorization'] = `Bearer ${refreshToken}`;
-                const response = await axios.post('http://127.0.0.1:8009/refresh', { refresh_token: refreshToken });
-                const newToken = response.data.access_token;
-                // nastavime header axiousu
-                axios.defaults.headers.common['Authorization'] = `Bearer ${newToken}`;
-
-                localStorage.setItem('token', newToken);
-                this.token = newToken;
-                console.log('Token refreshed');
-            } catch (error) {
-                localStorage.removeItem('token');
-                throw new Error('Failed to refresh token');
             }
         },
         logout() {
