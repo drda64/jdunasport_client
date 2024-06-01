43ade16097b8a64df4f77a78d6b8d3a2797434f5
Šimon Drda <drdasimo@fel.cvut.cz>
2024-06-01 22:04:53 +0200

    add functions to event view

--- src/views/EventView.vue
+++ src/views/EventView.vue
@@ -1,7 +1,10 @@
 <script setup>
+import Swal from 'sweetalert2'
 import { ref, onMounted } from 'vue';
-import EventItem from "@/models/EventItem.js";
-import Category from "@/models/Category.js";
+import EventModel from "@/models/EventModel.js";
+import CategoryModel from "@/models/CategoryModel.js";
+import TrashIcon from "@/icons/TrashIcon.vue";
+import router from "@/router/index.js";
 
 // Define props
 const props = defineProps({
@@ -11,6 +14,8 @@ const props = defineProps({
 // Reactive variable to hold the event item data
 const eventItem = ref(null);
 const categories = ref([]);
+const userInEvent = ref("");
+const categoryOfUser = ref(0)
 
 // Fetch event item data when the component is mounted
 onMounted(async () => {
@@ -18,41 +23,105 @@ onMounted(async () => {
 });
 
 async function fetchEventData() {
-  eventItem.value = await EventItem.getEventById(props.id);
-  categories.value = await Category.getCategoriesById(props.id);
+  eventItem.value = await EventModel.getEventById(props.id);
+  categories.value = await CategoryModel.getCategoriesById(props.id);
+
+  // check if user in category.participants
+  const object = await EventModel.isUserInEvent(props.id);
+  userInEvent.value = object.username;
+  categoryOfUser.value = object.category_id;
 }
 
 async function joinEvent(categoryId) {
+  if (userInEvent.value) {
+    const leftEvent = await leaveEvent(categoryId);
+    if (!leftEvent) {
+      // User canceled the leave action, so don't proceed with joining.
+      return;
+    }
+  }
+
   console.log('Joining event', categoryId);
-  await EventItem.joinEvent(props.id, categoryId);
+  await EventModel.joinEvent(props.id, categoryId);
   await fetchEventData();
+}
+
+function leaveEvent(categoryId) {
+  return new Promise((resolve) => {
+    Swal.fire({
+      title: 'Opravdu chcete opustit pozici?',
+      showCancelButton: true,
+      confirmButtonText: 'Ano',
+      confirmButtonColor: '#f54646',
+      cancelButtonText: 'Ne',
+      icon: 'error',
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        console.log('Leaving event', categoryId);
+        await EventModel.leaveEvent(props.id, categoryId);
+        await fetchEventData();
+        resolve(true);  // User confirmed leaving
+      } else {
+        resolve(false); // User canceled the leave action
+      }
+    });
+  });
+}
+
+function deleteEvent() {
+  Swal.fire({
+    title: 'Opravdu chcete smazat event?',
+    showCancelButton: true,
+    confirmButtonText: 'Ano',
+    confirmButtonColor: '#f54646',
+    cancelButtonText: 'Ne',
+    icon: 'error',
+  }).then(async (result) => {
+    if (result.isConfirmed) {
+      console.log('Deleting event', props.id);
+      await EventModel.deleteEvent(props.id);
+      await router.push('/');
+    }
+  });
 }
 
 </script>
 
 <template>
   <div v-if="eventItem">
-    <h1>{{ eventItem.name }}</h1>
-    <p><mark>{{ eventItem.sport }}</mark></p>
-    <p class="description">/ {{ eventItem.description }}</p>
-    <p>/ {{ eventItem.date }}</p>
-    <p>/ {{ eventItem.time }}</p>
-    <p>/ {{ eventItem.location }}</p>
+    <header id="event">
+      <div class="info">
+        <h1>{{ eventItem.name }}</h1>
+        <p><mark>{{ eventItem.sport }}</mark></p>
+        <p class="description">/ {{ eventItem.description }}</p>
+        <p>/ {{ eventItem.date }}</p>
+        <p>/ {{ eventItem.time }}</p>
+        <p>/ {{ eventItem.location }}</p>
+      </div>
+      <TrashIcon @click="deleteEvent" v-if="eventItem.created_by_user"></TrashIcon>
+    </header>
 
     <div v-for="category in categories" :key="category.id">
-      <header>
+      <header class="category">
         <h2>{{ category.name }}</h2>
-        <p class="add" @click="joinEvent(category.id)">Přidat se jako {{ category.name.toLowerCase() }}</p>
+        <p v-if="!userInEvent" class="add" @click="joinEvent(category.id)">Přidat se jako {{ category.name.toLowerCase() }}</p>
+        <p v-if="userInEvent && category.id !== categoryOfUser" class="add" @click="joinEvent(category.id)">Přidat se jako {{ category.name.toLowerCase() }}</p>
+        <p v-else-if="userInEvent && category.id === categoryOfUser" class="leave" @click="leaveEvent(category.id)">Opustit pozici</p>
       </header>
       <table>
         <tr>
-          <th>Jméno</th>
+          <th>Uživatel</th>
+          <th class="date">Přidán</th>
         </tr>
-        <tr v-for="participant in category.participants" :key="participant.id">
-          <td>{{ participant.name }}</td>
+        <tr :class="participant.name === userInEvent ? 'user' : '' " v-for="participant in category.participants" :key="participant.id">
+          <td v-if="participant.substitute">{{ participant.name }} - náhradník</td>
+          <td v-else>{{ participant.name }}</td>
+
+          <td class="date">{{ participant.created_at.toLocaleDateString() }}</td>
         </tr>
-        <tr v-for="n in (category.capacity - category.participants.length)" :key="`empty-${n}`">
+        <tr v-if="(category.capacity - category.participants.length) > 0" v-for="n in (category.capacity - category.participants.length)" :key="`empty-${n}`">
           <td>-</td>
+          <td class="date">-</td>
         </tr>
       </table>
     </div>
@@ -72,11 +141,17 @@ h2 {
   margin: 0;
 }
 
-header {
+header#event {
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+}
+
+header.category {
   margin-top: 2rem;
   display: flex;
   justify-content: space-between;
-  align-items: center;
+  align-items: flex-end;
 }
 
 div > p:first-of-type, p.description {
@@ -87,7 +162,11 @@ div > p:nth-of-type(n+3) {
   margin-bottom: 0.1rem;
 }
 
-.add:hover {
+.leave {
+  color: #f54646;
+}
+
+.add:hover, .leave:hover {
   text-decoration: underline;
   cursor: pointer;
 }
@@ -97,23 +176,36 @@ table {
   border-collapse: collapse;
 
   margin-top: 0.5rem;
+
+  font-size: 0.9rem;
 }
 
 td {
   border:1px solid #e5e5e5;
 }
 
+p {
+  word-break: break-word;
+}
+
+.date {
+  width: 100px;
+}
+
 th, td {
   text-align: left;
-  font-weight: 500;
   padding: 0.5rem;
 }
 
-th {
-  background-color: #5EC4B1;
-  border: none;
+tr.user {
+  background-color: #f0f0f0;
+}
 
-  margin-bottom: 0.5rem;
+th {
+  background-color: #3a3db2;
+  border: none;
+  color: white;
+  font-weight: 500;
 }
 
 td {
