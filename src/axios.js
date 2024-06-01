4af9972cc54d802172b03aa827098c2a0730a879
Šimon Drda <drdasimo@fel.cvut.cz>
2024-06-01 22:07:14 +0200

    error when no server conn

--- src/axios.js
+++ src/axios.js
@@ -2,6 +2,7 @@
 
 import axios from 'axios';
 import { useTokenStore } from '@/stores/token.js';
+import {notify} from "@kyvg/vue3-notification";
 
 axios.defaults.baseURL = 'http://localhost:8009';
 
@@ -14,5 +15,15 @@ axios.interceptors.request.use(config => {
     }
     return config;
 }, error => {
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(response => {
+    return response;
+}, error => {
+    if (!error.response) {
+        // Network error (server not reachable, etc)
+        notify('Chyba připojení k serveru', 'error');
+    }
     return Promise.reject(error);
 });
