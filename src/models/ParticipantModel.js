78dfbbc0346a4422e1cb7c3409c30a26cfa9ecc9
Šimon Drda <drdasimo@fel.cvut.cz>
2024-06-01 22:03:23 +0200

    creating participan model

---
+++ src/models/ParticipantModel.js
@@ -0,0 +1,8 @@
+export class ParticipantModel {
+    constructor(id, name, substitute, created_at) {
+        this.id = id;
+        this.name = name;
+        this.substitute = substitute;
+        this.created_at = created_at;
+    }
+}
