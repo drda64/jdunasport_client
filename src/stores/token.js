e3624386fd5486336e18147aa4c0848c3f9463c4
Šimon Drda <drdasimo@fel.cvut.cz>
2024-06-01 22:07:27 +0200

    adding headers

--- src/stores/token.js
+++ src/stores/token.js
@@ -1,28 +1,23 @@
+// src/stores/token.js
+
 import { defineStore } from 'pinia';
 import axios from 'axios';
 
 export const useTokenStore = defineStore('auth', {
-    // define the store properties
     state: () => ({
         token: localStorage.getItem('token') || '',
-        refresh_token: localStorage.getItem('refresh_token') || '',
     }),
-    // we only need one getter
     getters: {
         isAuthenticated: (state) => !!state.token,
-
     },
-    // two actions for login and logout
     actions: {
         async login(user) {
             try {
-                const response = await axios.post('http://127.0.0.1:8009/login', user);
+                const response = await axios.post('/login', user);
                 const token = response.data.access_token;
                 localStorage.setItem('token', token);
                 this.token = token;
-
-                console.log('Login successful');
-                console.log(token);
+                axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
             } catch (error) {
                 localStorage.removeItem('token');
                 throw new Error('Invalid username or password');
