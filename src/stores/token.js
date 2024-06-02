4cc423f858c44d33161b6e1194c05d4f00a50ed8
Šimon Drda <drdasimo@fel.cvut.cz>
2024-06-02 17:57:35 +0200

    new registration method

--- src/stores/token.js
+++ src/stores/token.js
@@ -2,7 +2,9 @@
 
 import { defineStore } from 'pinia';
 import axios from 'axios';
+import router from "@/router/index.js";
 
+// store for storing jwt token
 export const useTokenStore = defineStore('auth', {
     state: () => ({
         token: localStorage.getItem('token') || '',
@@ -11,18 +13,53 @@ export const useTokenStore = defineStore('auth', {
         isAuthenticated: (state) => !!state.token,
     },
     actions: {
+        // login func
         async login(user) {
             try {
+                // posting the user data to the server
                 const response = await axios.post('/login', user);
+
+                // setting the token to the local storage and to the store
+                const token = response.data.access_token;
+                localStorage.setItem('token', token);
+                this.token = token;
+
+                // setting the token to the axios headers
+                axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+            } catch (error) {
+                // removing the token from the local storage
+                localStorage.removeItem('token');
+
+                // catching error
+                if (error.response && error.response.data && error.response.data.message) {
+                    // Return the actual error message from the response
+                    return error.response.data.message
+                } else {
+                    // Fallback error message
+                    return 'Něco se pokazilo'
+                }
+            }
+        },
+        async register(user) {
+            try {
+                const response = await axios.post('/register', user);
                 const token = response.data.access_token;
                 localStorage.setItem('token', token);
                 this.token = token;
                 axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+                return null; // Indicate success with no error
             } catch (error) {
                 localStorage.removeItem('token');
-                throw new Error('Invalid username or password');
+
+                console.error(error.response?.data);
+                if (error.response && error.response.data) {
+                    return error.response.data;
+                } else {
+                    return 'Něco se pokazilo...';
+                }
             }
         },
+        // logout function
         logout() {
             this.token = '';
             localStorage.removeItem('token');
