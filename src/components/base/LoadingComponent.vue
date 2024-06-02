816bcfa7dd61a5c35e965271de154a658ec46dc1
Šimon Drda <drdasimo@fel.cvut.cz>
2024-06-02 20:17:46 +0200

    loading component

---
+++ src/components/base/LoadingComponent.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref } from 'vue';
+import { useLoaderStore} from "@/stores/loader.js";
+
+const loader = useLoaderStore();
+</script>
+
+<template>
+  <div v-if="loader.loading" class="loading-overlay">
+    <div class="spinner"></div>
+  </div>
+</template>
+
+<style scoped>
+.loading-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 9999;
+}
+
+.spinner {
+  border: 8px solid #f3f3f3;
+  border-top: 8px solid var(--primary-color);
+  border-radius: 50%;
+  width: 36px;
+  height: 36px;
+  animation: spin 2s linear infinite;
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+</style>
